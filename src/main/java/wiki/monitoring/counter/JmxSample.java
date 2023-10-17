3329eee44c9e54675f7749456704e2539a5ef659
Aleksandr Romanenkov <91538748+43034r@users.noreply.github.com>
2023-10-17 10:50:12 +0300

    upd dockerfile upd controller name

--- src/main/java/wiki/monitoring/counter/JmxSample.java
+++ src/main/java/wiki/monitoring/counter/JmxSample.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackageClasses = RestServiceCounter.class)
 @EnableScheduling
 public class JmxSample {
 public static void main(String[] args) {
